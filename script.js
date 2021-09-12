f859a599fd23e4c0ee3622b7c7b648a1c9829af2
StevenLance <44912347+stevenjlance@users.noreply.github.com>
2021-09-12 21:39:45 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function setup() {
+  createCanvas(400, 400);
+  angleMode(DEGREES) //this makes working with arcs easier, since we don't all have radians and the unit circle memorized.
+
+}
+
+function draw() {
+  background(220);
+  text(mouseX + ", " + mouseY, 20,20)
+
+  //base face shape
+  arc(200,250,300,350,180,0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
